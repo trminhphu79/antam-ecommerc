46e43131dfc1f06b1983354c4dbd10c5495141fc
minhphu-appvity <asagosv2@gmail.com>
2022-02-15 22:42:24 +0700

    update code
    update code

--- src/app/page/product-detail/index.js
+++ src/app/page/product-detail/index.js
@@ -2,9 +2,8 @@ import React from "react";
 import "./ProductDetail.scss";
 export const ProductDetail = () => {
     let phudz = "";
-    return <div className="ProductDetailList" >
-
-            Chi tiet san pham
+    return <div className="ProductDetail" >
+        <div className="child">Class CHild</div>
     </div>;
 }
 
