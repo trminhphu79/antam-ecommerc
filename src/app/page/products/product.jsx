0a2748ed390ca6d68bc8b3f962ebe205e9fa79a0
Hong Linh <71358321+lelinh014756@users.noreply.github.com>
2022-03-02 15:40:25 +0700

    update pagination
    update component + logic pagination

--- src/app/page/products/product.jsx
+++ src/app/page/products/product.jsx
@@ -1,23 +1,39 @@
-import React, { Component } from 'react';
-import Filter from './filter' ; 
-import ProductContainer from './productContainer';
-import './style/product.css' ; 
-import { getProducts } from '../server/productChaillo';
+import React, { Component } from "react";
+import Filter from "./filter";
+import ProductContainer from "./productContainer";
+import "./style/product.css";
+import { getProducts } from "../server/productChaillo";
+import { paginate } from "../common/pagination/paginate/paginate";
 
 class Products extends Component {
-    state = { 
-        products : getProducts()
-    }
+  state = {
+    products: getProducts(),
+    pageSize: 8,
+    currentPage: 1,
+  };
 
-    render() { 
-        const { products } = this.state ;
-        return (
-            <div className="background-page product">
-                <Filter />
-                <ProductContainer products={products}/>
-            </div>
-        );
-    }
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+
+  render() {
+    const { products, currentPage, pageSize, sortProduct } = this.state;
+    const { length: itemsCount } = products;
+
+    const allProduct = paginate(products, currentPage, pageSize);
+    return (
+      <div className="background-page product">
+        <Filter />
+        <ProductContainer
+          products={allProduct}
+          itemsCount={itemsCount}
+          currentPage={currentPage}
+          pageSize={pageSize}
+          onPageChange={this.handlePageChange}
+        />
+      </div>
+    );
+  }
 }
- 
+
 export default Products;
--- src/app/page/products/productContainer.jsx
+++ src/app/page/products/productContainer.jsx
@@ -1,11 +1,24 @@
 import React from 'react' ; 
 import ListProduct from './listProduct';
+import Pagination from '../common/pagination/pagination';
 
-const ProductContainer = ({ products }) => {
+const ProductContainer = (props) => {
+    const {
+        products: allProduct,
+        currentPage,
+        pageSize , 
+        itemsCount,
+        onPageChange } = props;
 
     return ( 
         <div className="product-container">
-            <ListProduct products={products}/>
+            <ListProduct products={allProduct} />
+            <Pagination
+                currentPage={currentPage}
+                itemsCount={itemsCount}
+                onPageChange={onPageChange}
+                pageSize={pageSize}
+            />
         </div>
      );
 }
