0f3edf74e8b12318f61a43b07b3a574b83dbe21a
Hong Linh <71358321+lelinh014756@users.noreply.github.com>
2022-03-02 07:26:01 +0700

    update navbar
    Update logic active navbar item

--- src/app/components/navbar/index.js
+++ src/app/components/navbar/index.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.scss";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import { listItemNavbar } from "./data/list-item-navbar";
 
 export const Navbar = () => {
+
+  /* Logic active navbar item */
+  const [active, setActive] = useState(1);
+
+  const CurrentActive = (id) => {
+    if(active === id) {
+      return " active"
+    } else {
+      return '';
+    }
+  }
+
   return (
     <nav className="navbar">
       <ul className="navbar__list">
-      {listItemNavbar.map(item => (
-        <Link key={item.id} to={item.link} className="navbar__item">
-          <i className="navbar__item-icon">
-            {item.icon}
-          </i>
-          <p className="navbar__item-name">{item.itemName}</p>
-        </Link>
-      ))}
+        {listItemNavbar.map((item) => (
+          <Link
+           key={item.id} 
+           to={item.link} 
+           className={"navbar__item " + (CurrentActive(item.id))} 
+           onClick={() => setActive(item.id)} >
+            <i className="navbar__item-icon">{item.icon}</i>
+            <p className="navbar__item-name">{item.itemName}</p>
+          </Link>
+        ))}
       </ul>
     </nav>
   );
