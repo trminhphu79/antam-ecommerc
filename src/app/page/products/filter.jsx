6e50237d42eea4b947a743c0a7959e8a8f84ca42
dat dang <38239103+traxanhkho@users.noreply.github.com>
2022-03-02 20:25:19 +0700

    update sorting products
    i love you <3

--- src/app/page/products/filter.jsx
+++ src/app/page/products/filter.jsx
@@ -1,58 +1,35 @@
 import React, { Component } from 'react';
-import _, { camelCase } from "lodash";
+import _ from "lodash";
 // waiting for sort featered in process.... 
 
 class Filter extends Component {
+    raiseSort = (button) => {
+        let sortProduct = { ...this.props.sortProduct };
 
-    state = {
-        selected: 'price-asc',
-    }
+        sortProduct.id = button.id ; 
+        sortProduct.order = button.order ; 
+        sortProduct.path = button.path ; 
 
-    raiseSort = (items) => {
-        this.setState({ selected: items.name })
-        const sortProduct = { ...this.props.sortProduct }
-        switch (items.name) {
-            case "price-asc": console.log('abc');
-            case "price-desc": console.log('xyz');
-            case "title-asc": console.log('4wd');
-            case "title-desc": console.log('rfef');
-        }
+        this.props.onSort(sortProduct) ;  
     }
 
     render() {
-        const { sortProduct, buttons: btn } = this.props;
+        const { buttons , sortProduct } = this.props;
 
         return (
             <div className='filter'>
                 <h6>sắp xếp theo :</h6>
                 <div className="wrapper">
-                    <button
-                        className={this.getButtonClass(_.get(btn, btn.priceAsc.name))}
-                        onClick={() => this.raiseSort(btn.priceAsc)}>
-                        giảm - tăng
-                    </button>
-                    <button
-                        className={this.getButtonClass(_.get(btn, btn.priceDesc.name))}
-                        onClick={() => this.raiseSort(btn.priceDesc)}
-                    >tăng - giảm</button>
-                </div>
-                <span>|</span>
-                <div className='wrapper'>
-                    <button
-                        className={this.getButtonClass(_.get(btn, btn.titleAsc.name))}
-                        onClick={() => this.raiseSort(btn.titleAsc)}
-                    >A - Z</button>
-                    <button
-                        className={this.getButtonClass(_.get(btn, btn.titleDesc.name))}
-                        onClick={() => this.raiseSort(btn.titleDesc)}
-                    >Z - A</button>
+                    {buttons.map(button => (
+                        <button
+                            className={sortProduct.id === button.id ? 'active' : ''}
+                            key={button.id}
+                            onClick={() => this.raiseSort(button)}
+                        >{ button.title }</button>
+                    ))}
                 </div>
             </div>
         );
-    }
-
-    getButtonClass = (name) => {
-        return this.state.selected === name ? 'active' : '';
     }
 }
 
--- src/app/page/products/product.jsx
+++ src/app/page/products/product.jsx
@@ -5,55 +5,65 @@ import ProductContainer from './productContainer';
 import './style/product.css' ; 
 import { getProducts } from '../fakeServer/productChaillo';
 import { paginate } from '../utils/paginate' ; 
+import _ from 'lodash'
 
 class Products extends Component {
     state = {
         products : getProducts(), 
         pageSize : 8 , 
         currentPage : 1 , 
-        sortProduct : { path : 'priceHight' , order : 'asc' },
-        selectedBtn : true ,
+        sortProduct : { id : 'asc322ssaf1' , order : 'asc' , path : 'priceLow' },
     }
 
-    buttons = {
-        priceAsc : {
-            name: 'price-asc' , 
-            path : 'priceHight' , 
+    buttons = [
+        {
+            id : 'asc322ssaf1' , 
+            title : 'giảm - tăng',
+            path : 'priceLow' , 
+            order : 'asc'
         },
-        priceDesc : {
-            name: 'price-desc' , 
-            path : 'priceHight' , 
+        {
+            id : 'asc322ssa31' , 
+            title : 'tăng - giảm',
+            path : 'priceLow' ,
+            order : 'desc' 
         },
-        titleAsc : {
-            name: 'title-asc' , 
+        {
+            id : 'asc322ssa21' , 
+            title : 'A - Z',
             path : 'title' , 
+            order : 'asc'
         },
-        titleDesc : {
-            name: 'title-desc' , 
-            path : 'title' ,
+        {
+            id : 'asc32ss1af1' , 
+            title : 'Z - A',
+            path : 'title' , 
+            order : 'desc' 
         },
-        
-    }
+    ]
 
     handlePageChange = (page) => {
         this.setState({ currentPage : page }) ; 
     }
 
-    handleSortProduct = (path) =>{
-
+    handleSortProduct = (sortProduct) =>{
+        this.setState({ sortProduct }) ; 
     }
 
     render() { 
         const { products , currentPage , pageSize , sortProduct} = this.state ;
         const { length : itemsCount } = products ; 
 
-        const allProduct = paginate(products,currentPage,pageSize) ;
+        const sorted = _.orderBy(products, [sortProduct.path] , [sortProduct.order]) ; 
+
+        const allProduct = paginate(sorted ,currentPage,pageSize) ;
         
         return (
             <div className="product">
                 <Filter 
                     sortProduct={sortProduct}
                     buttons={this.buttons}
+                    onSort={this.handleSortProduct}
                 />
                 <ProductContainer 
                     products={allProduct}
