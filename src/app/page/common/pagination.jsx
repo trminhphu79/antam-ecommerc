fb62ca8f08efb572f38e2b4409b791a49d81174d
dat dang <38239103+traxanhkho@users.noreply.github.com>
2022-03-05 20:10:46 +0700

    update style of pagination component

--- src/app/page/common/pagination.jsx
+++ src/app/page/common/pagination.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import _ from 'lodash' ; 
 import PropTypes from 'prop-types' ; 
+import './styles/pagination.css' ; 
 
 const Pagination = ({ itemsCount , pageSize , currentPage , onPageChange }) => {
     const pagesCount = Math.ceil(itemsCount / pageSize) ; 
