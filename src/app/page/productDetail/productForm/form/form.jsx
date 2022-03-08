dfdedf5ba527b36247c433fd571a55b2614c3c64
Hong Linh <71358321+lelinh014756@users.noreply.github.com>
2022-03-08 22:21:30 +0700

    Revert "Merge branch 'Dat_Logics' into Linh_UI/IUX"
    This reverts commit bdd444f0cc47fbc2bd62d369d60b7ad14cb806fd, reversing
    changes made to 5b3145e6c6a532687d79601c17833b369bbf9f95.

--- src/app/page/productDetail/productForm/form/form.jsx
+++ src/app/page/productDetail/productForm/form/form.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
-const Form = ({forms}) => {
+const Form = (props) => {
+    const { forms } = props ; 
     return ( 
-        <form action="" id="form-1" className='col-lg-6 col-md-6 col-xs-12'>
+        <form action="" className='col-lg-6 col-md-6 col-xs-12'>
             <div className="wrapper">
                 <h2>nhập thông tin đặt hàng</h2>
                 {forms.map(f => (
-                    <div className="input-group" key={f._id}>
+                    <div className="input-group" key={f.name}>
                         <label htmlFor={f.name}>{f.name}</label>
                         <input type={f.type} placeholder={f.placeholder} />
                     </div>
                 ))}
             </div>
+            
         </form>
     );
 }
