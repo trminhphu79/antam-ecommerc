87d731ad22c35ec7e4f09454399265304ff023df
dat dang <38239103+traxanhkho@users.noreply.github.com>
2022-03-10 16:51:15 +0700

    building create page

---
+++ src/app/page/admin/admin-page/create-page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CreatePage = () => {
+  return (
+    <div className="row">
+      <div className="col info">
+          </div>
+      <div className="col image">
+          </div>
+    </div>
+  );
+};
+
+export default CreatePage;
