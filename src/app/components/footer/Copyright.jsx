a858c7e94c7a976a38f3b17929734c2f4ad778a8
tuanduong1209 <tuanduong19077@gmail.com>
2022-04-01 00:19:35 +0700

    tuan-update
    tuan-update

---
+++ src/app/components/footer/Copyright.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Copyright = () => {
+  return (
+    <section className="copyright">
+      <span>
+        Copyright © 2022{" "}
+        <a href="https://chailoduocmyphamgiagoc.com/">
+          chailoduocmyphamgiagoc.com
+        </a>
+      </span>
+    </section>
+  );
+};
+
+export default Copyright;
