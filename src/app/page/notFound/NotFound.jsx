8a0b7f92cd0331b62f23c4fc1b2f72bfd209a3e4
tuanduong1209 <tuanduong19077@gmail.com>
2022-04-10 18:55:20 +0700

    tuan update
    tuan update

--- src/app/page/notFound/NotFound.jsx
+++ src/app/page/notFound/NotFound.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NotFound.scss";
+import { BsArrowLeft } from "react-icons/bs";
 
 const NotFound = () => {
-    return ( <h1>NotFound 404 page</h1> );
-}
- 
+  return (
+    <section className="notfound">
+      <img src="/404.png" alt="" className="" />
+      <Link to="/" className="notfound-btn btn">
+        <BsArrowLeft></BsArrowLeft>
+        <span>Về lại trang chủ</span>
+      </Link>
+    </section>
+  );
+};
+
 export default NotFound;
